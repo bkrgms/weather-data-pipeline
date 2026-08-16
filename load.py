fc67e036cc501f0250e60bde166d47864412646d
Bekir Gümüş <bekir.ggumus@gmail.com>
2026-08-16 23:15:51 +0300

    improve pipeline error handling

--- load.py
+++ load.py
@@ -1,14 +1,35 @@
 import psycopg
 import os
+import logging 
+
 from dotenv import load_dotenv
 load_dotenv()
-def save_weather_data(weather_records):
-    connection = psycopg.connect(
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    host=os.getenv("localhost"),
-    port=os.getenv("DB_PORT")
-)
+def save_weather_data(weather_records):  
+    db_name = os.getenv("DB_NAME")
+    db_user = os.getenv("DB_USER")
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+
+    if not db_name:
+        raise ValueError("DB_NAME environment veriable is missing")
+    if not db_user:
+        raise ValueError("DB_USER environment veriable is missing")
+    if not db_host:
+        raise ValueError("DB_HOST environment variable is missing")
+    if not db_port:
+        raise ValueError("DB_PORT environment variable is missing")
+    
+    try:
+        connection = psycopg.connect(
+            dbname=db_name,
+            user=db_user,
+            host=db_host,
+            port=db_port
+        )
+
+    except psycopg.Error as error:
+        logging.error("Database connection failed : %s",error)
+        raise
 
     cursor = connection.cursor()
 
